6006c437d05e7fc5593c277b12851fded5fe9bda
Ahmad <sharafedin.ahmad@gmail.com>
2019-07-23 11:36:42 +0300

    jQuery1 solve

--- jQuery-1/jQuery.js
+++ jQuery-1/jQuery.js
@@ -3,17 +3,49 @@ Q1:
 Check if jQuery is loaded
 */
 
+$(document).ready(function(){
+    if (jQuery){
+    // alert("jQuery is loaded"); 
+    }
+} );
+
 
 /*
 Q2:
 If You would have very long website - How to help user with fast getting back on top :)
 */
 
-
+$(document).ready(function (){
+    $("#button3").click(function (){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 4000);
+    });
+});
 /*
 Q3:
 If You would like to change sizes of Your div/button with on click :)
 */
+
+$(document).ready(function(){
+    var height = $('#box').height();
+    var width = $('#box').width();
+
+    $('#btn1').on('click', function() {
+        $('#box').height('300px');
+        $('#box').width('300px');
+    });
+
+    $('#btn2').on('click', function() {
+        $('#box').height(height);
+        $('#box').width(width);
+    });
+});
+
+
+
+
+
 
 
 /*
@@ -21,11 +53,30 @@ Q4:
 Learn how to fadein your boxes
 */
 
+$(document).ready(function(){
+    $("#delay").on('click',function(){
+    
+      $("#div1").fadeIn();
+      $("#div2").fadeIn("slow");
+      $("#div3").fadeIn(3000);
+    });
+  });
+
+
+
+
+
 
 /*
 Q5:
 User must first accept then he or she can signup
 */
+
+$(document).ready(function () {
+        $("#accept").on('click', function () {
+        $("#submitbtn").attr("disabled", !this.checked);
+    });
+});
 
 
 /*
@@ -33,17 +84,32 @@ Q6:
 Let them print
 */
 
+$(document).ready(function () {
+    $('.printPage').on('click' , function(){
+        window.print();
+    });
+    })
 
 /*
 Q7:
 Can username be too long? Yest it can - lets limit that
 */
-
+var maxLength = 20;
+$('#rchars').text(20);
+$('textarea').attr('maxLength', 20)
+$('textarea').on('keyup' ,function() {
+  var textlen = maxLength - $(this).val().length;
+  $('#rchars').text(textlen);
+});
 
 /*
 Q8:
 Wanna make some words bolder, do it with jQuery
 */
+
+
+
+
 
 
 /*
@@ -52,10 +118,16 @@ Add new div to your website
 */
 
 
+
+
+
 /*
 Q10:
 Select values from a JSON object using jQuery
 */
+
+
+
 
 
 /*
@@ -64,10 +136,16 @@ Add element within ul list
 */
 
 
+
+
+
 /*
 Q12:
 Know what user puts into textbox
 */
+
+
+
 
 
 /*
@@ -77,5 +155,10 @@ Change input value
 
 
 
+
+
 // if you finish try a new thing by your self
 // or go and repeat the DOM exercises using jQuery 
+
+
+
